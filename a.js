e3a6e76bad021ee3950229b3386e2fab53a9c4d8
Roque Fernando <eukor.roque@gmail.com>
2022-07-27 14:54:20 -0300

    ww

--- a.js
+++ a.js
@@ -1,11 +1,7 @@
 //Jogo par impar com pontos
 let mensagem
-let pontos
 function jogoParImpar() {
     n ? mensagem = "Jogar!" : null
     return mensagem
 }
 
-function pontuacao() {
-    pontos ++
-}
